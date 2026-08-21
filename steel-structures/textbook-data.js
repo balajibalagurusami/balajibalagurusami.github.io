5b65f4b3aa270f19efe696051c1d1881daacaee4
Balaji Balagurusami <121113302+balajibalagurusami@users.noreply.github.com>
2026-08-21 08:16:37 +0530

    Load Chapter 5 Steel Quest textbook lessons

--- steel-structures/textbook-data.js
+++ steel-structures/textbook-data.js
@@ -1,1 +1,1 @@
-document.write('<script src="textbook-data-base.js"><\/script><script src="textbook-data-ch4.js"><\/script>');
+document.write('<script src="textbook-data-base.js"><\/script><script src="textbook-data-ch4.js"><\/script><script src="textbook-data-ch5.js"><\/script>');
