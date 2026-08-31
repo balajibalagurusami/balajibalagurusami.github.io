33dd0b6166151067d304bfba8f80beda9bb6d44b
Balaji Balagurusami <121113302+balajibalagurusami@users.noreply.github.com>
2026-08-31 17:09:31 +0530

    Load Dynamics lecture 5 and 6 quiz lessons

--- dynamics-of-structures/lessons.js
+++ dynamics-of-structures/lessons.js
@@ -1,1 +1,1 @@
-document.write('<script src="lessons-1.js"><\/script><script src="lessons-2.js"><\/script><script src="lessons-3.js"><\/script><script src="lessons-4.js"><\/script>');
+document.write('<script src="lessons-1.js"><\/script><script src="lessons-2.js"><\/script><script src="lessons-3.js"><\/script><script src="lessons-4.js"><\/script><script src="lessons-5.js"><\/script><script src="lessons-6.js"><\/script>');
